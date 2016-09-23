440384b9fb766270d5d12744f6e68d3b10efb087
cxmscb <1073216732@qq.com>
2016-09-23 15:51:53 +0800

    code

---
+++ app/src/main/java/com/cxmscb/cxm/videosplash/MainActivity.java
@@ -0,0 +1,21 @@
+package com.cxmscb.cxm.videosplash;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.widget.VideoView;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+}
